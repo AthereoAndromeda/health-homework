e1b3e0566466dfcc9f234d7a76bfa7701bb960a9
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-06-21 15:16:58 +0800

    Added SourceCards Component to easily add sources

---
+++ src/components/SourceCards.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="q-pa-md row items-center q-gutter-md">
+    <q-card v-for="content in contents" :key="content" bordered>
+      <q-card-section>
+        <div class="text-h6">
+          <a :href="content.link">
+            {{ content.title }}
+          </a>
+        </div>
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <q-card-section>
+        {{ content.author }}
+      </q-card-section>
+    </q-card>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface SourceCard {
+  title: string;
+  link: string;
+  author: string;
+}
+
+export default defineComponent({
+  name: 'SourceCards',
+  props: {
+    contents: {
+      type: Array as () => SourceCard[],
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.q-card {
+  background: $grey-4;
+  transition: 0.3s;
+}
+
+.q-card--dark {
+  background: $dark;
+  transition: 0.3s;
+}
+</style>
