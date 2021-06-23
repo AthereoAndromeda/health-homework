e48a35f7f00472c83e88dfc6768492c4c19b6b90
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-06-24 00:50:28 +0800

    Cards now expand and glow on hover

--- src/components/SourceCards.vue
+++ src/components/SourceCards.vue
@@ -6,32 +6,46 @@
     target="_blank"
     class="q-pa-none"
   >
-    <q-card bordered>
+    <q-card
+      bordered
+      @mouseenter="expanded = true"
+      @mouseleave="expanded = false"
+    >
       <!-- Title area -->
       <q-card-section>
         <div class="text-h6">{{ content.title }}</div>
       </q-card-section>
 
       <q-separator dark inset />
-
-      <q-card-section class="text-subtitle2">
-        {{ content.author }}
-      </q-card-section>
+      <q-slide-transition>
+        <div v-show="expanded">
+          <q-card-section class="text-subtitle2">
+            {{ content.author }}
+          </q-card-section>
+        </div>
+      </q-slide-transition>
     </q-card>
   </q-item>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { ref, defineComponent } from 'vue';
 import { SourceCard } from './models';
 
 export default defineComponent({
   name: 'SourceCards',
+
   props: {
     content: {
       type: Object as () => SourceCard,
       required: true,
     },
+  },
+
+  setup() {
+    return {
+      expanded: ref(false),
+    };
   },
 });
 </script>
@@ -46,13 +60,17 @@ h6,
   font-family: 'Poppins';
 }
 
-a {
-  text-decoration: none;
+.q-item {
+  transition: 0.3s;
 }
 
 .q-card {
   background: $grey-4;
   transition: 0.3s;
+
+  &:hover {
+    box-shadow: 3px 3px 15px $accent;
+  }
 }
 
 .q-card--dark {
